2ad853b3720eed05d1b05b1f5fb33e5b74237604
krshmt <122864621+krshmt@users.noreply.github.com>
2023-07-20 08:50:20 +0200

    Revert "modification du script"
    This reverts commit 42a6f708d5715d8c7f3f4540246b35a6cf2cdbf2.

--- JS/app.js
+++ JS/app.js
@@ -1,10 +1,10 @@
-new TypeWriter({
-    type_writer_element: document.body.querySelector('p[class="changement-text typed-effect-text"]'),
-    text_array: ["étudiant en deuxième année de BUT Informatique.", "en recherche d'une alternance.", "passionné par le développement WEB.", "fan de Formule 1."],
-    typing_delay: 30,
-    erasing_delay: 30,
-    new_text_delay: 1000
-});
+// new TypeWriter({
+//     type_writer_element: document.body.querySelector('p[class="changement-text typed-effect-text"]'),
+//     text_array: ["étudiant en deuxième année de BUT Informatique.", "en recherche d'une alternance.", "passionné par le développement WEB.", "fan de Formule 1."],
+//     typing_delay: 30,
+//     erasing_delay: 30,
+//     new_text_delay: 1000
+// });
 
 new TypeWriter({
     type_writer_element: document.body.querySelector('h2[class="changement-text typed-effect-text-deux"]'),
